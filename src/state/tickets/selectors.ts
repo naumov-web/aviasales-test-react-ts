242d1e4be7966f483c4fa74f395c6376ef34ea7b
Константин Наумов <konstantin@MacBook-Pro-Konstantin-2.local>
2019-10-20 12:56:37 +0300

    Filters by stops count

--- src/state/tickets/selectors.ts
+++ src/state/tickets/selectors.ts
@@ -8,18 +8,18 @@ export const getSort = (state: IState) : string => state.tickets.sort;
 export const getFilters = (state: IState) : Array<Number> => state.tickets.filters;
 
 export const getTransformedTickets = (state: IState) : Array<ITicket> => {
+  const filtered = filterByTransfersCount(state);
+
   if (state.tickets.sort === 'cheap') {
     return sortByField(
-      state.tickets.tickets,
+      filtered,
       'price',
       'asc'
     );
   }
   if (state.tickets.sort === 'fast') {
 
-    const result = state.tickets.tickets.map(item => item);
-
-    return result.sort(
+    return filtered.sort(
       (item1 : ITicket, item2 : ITicket) => {
         return getAvgDuration(item1) > getAvgDuration(item2) ? 1 : -1;
       }
@@ -36,3 +36,25 @@ const getAvgDuration = (ticket : ITicket) : Number => {
 
   return sum / ticket.segments.length;
 };
+
+const filterByTransfersCount = (state : IState) : Array<ITicket> => 
+  state.tickets.tickets.filter(
+    (item : ITicket) => {
+
+      const filters = state.tickets.filters;
+
+      if (filters.indexOf(1) !== -1 || filters.length === 0) {
+        return true;
+      }
+
+      let accepted = true;
+
+      item.segments.forEach((segment) => {
+        let stops_length = segment.stops.length;
+        
+        accepted = accepted && (filters.indexOf(stops_length + 2) !== -1);
+      });
+
+      return accepted;
+    }
+  );
